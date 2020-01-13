3bb04977afd921c69a94ba79cdbb2c1b8b4a452f
Iman Akbari <imakbari@gmail.com>
2020-01-13 13:18:03 -0500

    Allow spark configurations

--- kabab/utils/sprk.py
+++ kabab/utils/sprk.py
@@ -2,10 +2,10 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, SQLContext
 
 
-def get_spark_session(cores=64):
+def get_spark_session(cores=64, conf=None):
     # load Spark session
     spark = SparkSession.builder.master("local[%d]" % cores).appName("PySparkShell").getOrCreate()
-    conf = SparkConf().setAppName("PySparkShell").setMaster("local[%d]" % cores)
+    conf = conf or SparkConf().setAppName("PySparkShell").setMaster("local[%d]" % cores)
     sc = SparkContext.getOrCreate(conf)
     sqlContext = SQLContext(sc)
     return spark, sc, sqlContext
